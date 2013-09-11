4dc2b28393478f6ff54fff8fc43e3e94d63eef5f
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2013-09-11 10:33:10 +0200

    Rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Babatrip::Application.config.secret_key_base = '04c9eeafb91e1adb60113c519c6b1721d89493c1a292dfb59cc6b3ee925676a5e2ecc79176541274a445ea12e53949887d235ce9991401ed0a1da403ff11da66'
