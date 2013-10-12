ce39ae92f8de17b57fde650820f9a3cce17089ad
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2013-10-12 16:50:52 +0200

    f jquery

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Babatrip
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # from jquery-rails-cdn
+    config.assets.precompile += ['jquery.js']
   end
 end
