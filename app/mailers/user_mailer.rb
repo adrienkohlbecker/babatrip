92be3eb4673afbd7a1ad19f4cf6292e1f77b94d0
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2013-12-14 15:20:20 +0100

    Add mandrill settings

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,3 @@
+class UserMailer < ActionMailer::Base
+  default from: "Travel Meet <contact@travel-meet.com>"
+end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,16 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# mandrill heroku addon
+ActionMailer::Base.smtp_settings = {
+    :port =>           '587',
+    :address =>        'smtp.mandrillapp.com',
+    :user_name =>      ENV['MANDRILL_USERNAME'],
+    :password =>       ENV['MANDRILL_APIKEY'],
+    :domain =>         'heroku.com',
+    :authentication => :plain
+}
+ActionMailer::Base.delivery_method = :smtp
+
 # Initialize the Rails application.
 Babatrip::Application.initialize!
