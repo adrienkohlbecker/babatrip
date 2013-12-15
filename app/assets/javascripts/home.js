adc2bc10a582a2e5808973346fd8654b98e13d87
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2013-12-15 17:27:17 +0100

    Add chosen to profile

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,33 +2,36 @@
 // All this logic will automatically be available in application.js.
 //= require foundation-datepicker
 
+$(function(){
 
-var nowTemp = new Date();
-var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+  var nowTemp = new Date();
+  var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
 
-var checkin = $('#arriving').fdatepicker({
+  var checkin = $('#arriving').fdatepicker({
 
-  format: 'dd/mm/yyyy',
-  onRender: function (date) {
-    return date.valueOf() < now.valueOf() ? 'disabled' : '';
-  }
+    format: 'dd/mm/yyyy',
+    onRender: function (date) {
+      return date.valueOf() < now.valueOf() ? 'disabled' : '';
+    }
 
-}).on('changeDate', function (ev) {
+  }).on('changeDate', function (ev) {
 
-  if (ev.date.valueOf() > checkout.date.valueOf()) {
-    var newDate = new Date(ev.date);
-    newDate.setDate(newDate.getDate() + 1);
-    checkout.setDate(newDate);
-  }
-  checkin.hide();
-  $('#leaving')[0].focus();
+    if (ev.date.valueOf() > checkout.date.valueOf()) {
+      var newDate = new Date(ev.date);
+      newDate.setDate(newDate.getDate() + 1);
+      checkout.setDate(newDate);
+    }
+    checkin.hide();
+    $('#leaving')[0].focus();
 
-}).data('datepicker');
-var checkout = $('#leaving').fdatepicker({
-  format: 'dd/mm/yyyy',
-  onRender: function (date) {
-    return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
-  }
-}).on('changeDate', function (ev) {
-  checkout.hide();
-}).data('datepicker');
+  }).data('datepicker');
+  var checkout = $('#leaving').fdatepicker({
+    format: 'dd/mm/yyyy',
+    onRender: function (date) {
+      return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
+    }
+  }).on('changeDate', function (ev) {
+    checkout.hide();
+  }).data('datepicker');
+
+});
