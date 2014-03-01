482f48a57ebbdb13ba4e166f63fcee8a6af93d3c
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2014-03-01 13:34:56 +0100

    Better error messages in profile edit

--- app/controllers/me_controller.rb
+++ app/controllers/me_controller.rb
@@ -7,21 +7,22 @@ class MeController < ApplicationController
   end
 
   def edit
+    @user = current_user
   end
 
   def update
-    user = current_user
-    user.first_name = update_params[:user][:first_name]
-    user.last_name = update_params[:user][:last_name]
-    user.email = update_params[:user][:email]
+    @user = current_user
+    @user.first_name = update_params[:user][:first_name]
+    @user.last_name = update_params[:user][:last_name]
+    @user.email = update_params[:user][:email]
 
-    if not user.is_profile_completed
-      user.accepts = update_params[:user][:accepts]
+    if not @user.is_profile_completed
+      @user.accepts = update_params[:user][:accepts]
 
       if update_params[:share] == "1"
 
         url = "http://#{ENV['DOMAIN']}/"
-        graph = Koala::Facebook::API.new(user.facebook_token, ENV['FACEBOOK_APP_SECRET'])
+        graph = Koala::Facebook::API.new(@user.facebook_token, ENV['FACEBOOK_APP_SECRET'])
         begin
           graph.put_wall_post("I'm using Travel-Meet to share my trips and find out where my friends are going. Join me!", {:link => url})
         rescue => e
@@ -31,8 +32,8 @@ class MeController < ApplicationController
       end
     end
 
-    if user.email_changed?
-      user.is_email_overridden = true
+    if @user.email_changed?
+      @user.is_email_overridden = true
       # resend verification ?
     end
 
@@ -44,37 +45,37 @@ class MeController < ApplicationController
 
     if date
       if date < 18.years.ago
-        user.birth_date = date
+        @user.birth_date = date
       else
-        user.errors.add(:birth_date, "is too recent")
+        @user.errors.add(:birth_date, "is too recent")
       end
     elsif year == 0 and month == 0 and day == 0
-      user.birth_date = nil
+      @user.birth_date = nil
     else
-      user.errors.add(:birth_date, "is invalid")
+      @user.errors.add(:birth_date, "is invalid")
     end
 
-    user.sex = update_params[:user][:sex]
-    user.relationship_status = update_params[:user][:relationship_status]
-    user.mood = update_params[:user][:mood]
-    user.time = update_params[:user][:time]
+    @user.sex = update_params[:user][:sex]
+    @user.relationship_status = update_params[:user][:relationship_status]
+    @user.mood = update_params[:user][:mood]
+    @user.time = update_params[:user][:time]
 
-    user.description = update_params[:user][:description]
+    @user.description = update_params[:user][:description]
 
-    user.nationality = update_params[:user][:nationality]
-    user.city = update_params[:user][:city]
-    user.latitude = update_params[:user][:latitude]
-    user.longitude = update_params[:user][:longitude]
+    @user.nationality = update_params[:user][:nationality]
+    @user.city = update_params[:user][:city]
+    @user.latitude = update_params[:user][:latitude]
+    @user.longitude = update_params[:user][:longitude]
 
-    is_signup = !user.is_profile_completed
+    is_signup = !@user.is_profile_completed
 
-    user.is_profile_completed = true
+    @user.is_profile_completed = true
 
-    if user.errors.empty? && user.save
+    if @user.errors.empty? && user.save
       redir = is_signup ? root_path : my_profile_path
       redirect_to session.delete("user_return_to") || redir
     else
-      @profile = ProfileFacade.new(user)
+      flash[:notice] = @user.errors.full_messages.join(".\n")
       render 'edit'
     end
   end
