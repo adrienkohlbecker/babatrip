65abc988e25cf224a39e49ab54c1fb4f28ca0d86
Adrien Kohlbecker <adrien.kohlbecker@gmail.com>
2016-04-27 17:40:42 +0200

    Switch from mandrill to sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -4,11 +4,12 @@ require File.expand_path('../application', __FILE__)
 # mandrill heroku addon
 ActionMailer::Base.smtp_settings = {
     :port =>           '587',
-    :address =>        'smtp.mandrillapp.com',
-    :user_name =>      ENV['MANDRILL_USERNAME'],
-    :password =>       ENV['MANDRILL_APIKEY'],
-    :domain =>         'heroku.com',
-    :authentication => :plain
+    :address =>        'smtp.sendgrid.net',
+    :user_name =>      ENV['SENDGRID_USERNAME'],
+    :password =>       ENV['SENDGRID_PASSWORD'],
+    :domain =>         'travel-meet.com',
+    :authentication => :plain,
+    :enable_starttls_auto => true
 }
 ActionMailer::Base.delivery_method = :smtp
 
